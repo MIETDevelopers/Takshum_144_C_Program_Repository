b856b5251e15425ee36c0fd612d073f3ec200956
takshumbhau144 <76584897+takshumbhau144@users.noreply.github.com>
2021-03-18 20:56:18 +0530

    arithmetic.c

---
+++ arithmetic.c
@@ -0,0 +1,16 @@
+#include <stdio.h>//preprocessor directive to include standard input output function header file
+int main(){//main function body starts
+
+	int a=7,b=3,c;//variable declaration and value assignment
+	
+	c=a+b;//logic for adding two numbers
+	printf("a+b= %d\n",c);//statement to print addition output
+	c=a-b;//logic for subtracting two numbers
+	printf("a-b= %d\n",c);//statement to print subtraction output
+	c=a*b;//logic for multiplying two numbers
+	printf("a*b= %d\n",c);//statement to print multiplication output
+	c=a%b;//logic for remainder
+	printf("a%b= %d\n",c);//statement to print remainder output
+	
+	return 0;//return statement
+}//main function body ends
