607da96306d5e4e61dd34f116712219b3cbab724
takshumbhau144 <76584897+takshumbhau144@users.noreply.github.com>
2021-03-21 11:03:48 +0530

    Hello World Program

--- World.c
+++ World.c
@@ -1,7 +1,9 @@
-
-#include <stdio.h>
-int main() {
+//Author Name= Takshum Bhau
+//Creation Date=18-03-2021
+//Purpose= Hello World program
+#include <stdio.h>//preprocessor directive to include standard input output function header file
+int main() {//main function body starts
    // printf() displays the string inside quotation
    printf("Hello, World!");
-   return 0;
-}
+   return 0;//return statement
+}//main function body ends
